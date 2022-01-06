c149b0c183e723f1266ecb066486630bb1a977bd
Philimon 15-1-2021 <Philimon 15-1-2021@DESKTOP-RRNC74B>
2022-01-06 12:18:42 +0200

    Add project files.

---
+++ ElMal_ElHaram_BankingSystem/Client.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElMal_ElHaram_BankingSystem
+{
+    internal class Client
+    {
+        private int clientid;
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private DateTime birthdate;
+        private string email;
+        private string password;
+        private string phonenumber;
+        private string address;
+        private string gender;
+        private decimal balance;
+        protected string accounttype;
+
+        static Random random = new Random();
+        public Client() {}
+        public Client(int clientid, string firstname, string middlename, string lastname, int birthyear, int birthmonth, int birthday, string email, string phonenumber,
+
+             string address, string gender, decimal balance)
+        {
+            this.clientid = clientid;
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.lastname = lastname;
+            this.birthdate = new DateTime(birthyear,birthmonth,birthday);
+            this.email = email;
+            this.password = GeneratePassword();
+            this.phonenumber = phonenumber;
+            this.address = address;
+            this.gender = gender;
+            this.balance = balance;
+            this.accounttype = "Poor";
+        }
+        public int ClientID
+        {
+            get { return clientid; }
+        }
+        public string FirstName
+        {
+            set { firstname = value; }
+            get { return firstname; }
+        }
+        public string MiddleName
+        {
+            set { middlename = value; }
+            get { return middlename; }
+        }
+        public string LastName
+        {
+            set { lastname = value; }
+            get { return lastname; }
+        }
+        public DateTime Birthdate
+        {
+            set { birthdate = value; }
+            get { return birthdate; }
+        }
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+        public string Password
+        {
+            set { password = GeneratePassword(); }
+            get { return password; }
+        }
+        public string PhoneNumber
+        {
+            set { phonenumber = value; }
+            get { return phonenumber; }
+        }
+        public string Address
+        {
+            set { address = value; }
+            get { return address; }
+        }
+        public string Gender
+        {
+            set { gender = value; }
+            get { return gender; }
+        }
+        public decimal Balance
+        {
+            set { balance = value; }
+            get { return balance; }
+        }
+        public string AccountType 
+        { 
+            set { accounttype = value; }
+            get { return accounttype; } 
+        }
+        public static string GeneratePassword()
+        {
+            string p = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                int x = random.Next(1, 5);
+
+                if (x == 1)
+                {
+                    p += Convert.ToString(random.Next(0, 10));
+                }
+                else if (x == 2)
+                {
+                    p += char.ConvertFromUtf32(random.Next(65, 91));
+                }
+                else if (x == 3)
+                {
+                    p += char.ConvertFromUtf32(random.Next(97, 123));
+                }
+                else if (x == 4)
+                {
+                    p += char.ConvertFromUtf32(random.Next(33, 48));
+                }
+            }
+
+            return p;
+        }
+        public virtual decimal AddInterestToSavingsAccount(decimal balance)
+        {
+            return balance + 0.06m * balance;
+        }
+    }
+    internal class Premium : Client
+    {
+        public Premium() {}
+        public Premium(int clientid, string firstname, string middlename, string lastname, int birthyear, int birthmonth, int birthday, string email, string phonenumber,
+            string address, string gender, decimal balance)
+
+            : base(clientid, firstname, middlename, lastname, birthyear, birthmonth, birthday, email, phonenumber,address,gender, balance)
+
+        {
+            this.accounttype = "Premium";
+        }
+
+        public sealed override decimal AddInterestToSavingsAccount(decimal balance)
+        {
+            return balance + 0.09m * balance;
+        }
+    }
+    internal class Minor : Client
+    {
+        private bool ssnumber; // Social Security Number
+        public Minor() {}
+        public Minor(int clientid, string firstname, string middlename, string lastname, int birthyear, int birthmonth, int birthday, string email, string phonenumber,
+            string address, string gender, decimal balance, bool ssnumber)
+
+            : base(clientid, firstname, middlename, lastname, birthyear, birthmonth, birthday, email, phonenumber, address, gender, balance)
+
+        {
+            this.accounttype = "Minor";
+            this.ssnumber = ssnumber;
+        }
+        public bool SSN
+        {
+            set { ssnumber = value; }
+            get { return ssnumber; }
+        }
+    }
+}
---
+++ ElMal_ElHaram_BankingSystem/InvestmentCertificate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElMal_ElHaram_BankingSystem
+{
+    internal abstract class InvestmentCertificate
+    {
+        private int id;
+        private int num;
+        private decimal amount;
+        private DateTime startingdate;
+        private int yearsnum;
+        private DateTime expirydate;
+        public InvestmentCertificate() {}
+        public InvestmentCertificate(int id,int num, decimal amount, int currentyear, int currentmonth,int currentday, int yearsnum)
+        {
+            this.id = id;
+            this.num = num;
+            this.amount = amount;
+            this.startingdate = DateTime.Now;
+            this.yearsnum = yearsnum;
+            this.expirydate = startingdate.AddYears(yearsnum);
+        }
+
+        public abstract decimal AddInterest1(decimal amount);
+        public abstract decimal AddInterest2(decimal amount);
+        public abstract decimal AddInterest3(decimal amount);
+    }
+    internal class InvestmentCertificatePoor : InvestmentCertificate
+    {
+        public InvestmentCertificatePoor() {}
+        public InvestmentCertificatePoor(int id, int num, decimal amount, int currentyear, int currentmonth, int currentday, int yearsnum)
+            
+            : base(id, num, amount, currentyear, currentmonth, currentday, yearsnum) {}
+
+        public sealed override decimal AddInterest1(decimal amount)
+        {
+            return amount + 0.11m * amount;
+        }
+        public sealed override decimal AddInterest2(decimal amount)
+        {
+            return amount + 0.12m * amount;
+        }
+        public sealed override decimal AddInterest3(decimal amount)
+        {
+            return amount + 0.14m * amount;
+        }
+    }
+    internal class InvestmentCertificatePremium : InvestmentCertificate
+    {
+        public InvestmentCertificatePremium() {}
+        public InvestmentCertificatePremium(int id,  int num, decimal amount, int currentyear, int currentmonth, int currentday, int yearsnum)
+            
+            : base(id, num, amount, currentyear, currentmonth, currentday, yearsnum) {}
+
+        public sealed override decimal AddInterest1(decimal amount)
+        {
+            return amount + 0.14m * amount;
+        }
+        public sealed override decimal AddInterest2(decimal amount)
+        {
+            return amount + 0.15m * amount;
+        }
+        public sealed override decimal AddInterest3(decimal amount)
+        {
+            return amount + 0.17m * amount;
+        }
+    }
+}
